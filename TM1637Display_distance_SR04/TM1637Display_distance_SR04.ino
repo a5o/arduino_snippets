356b2f20c122547826a6c479445495b21c06cc61
Alberto Ferrarini <alberto.ferrarini@gmail.com>
2020-05-03 09:12:22 +0200

    Added TM1637Display_distance_SR04

---
+++ TM1637Display_distance_SR04/TM1637Display_distance_SR04.ino
@@ -0,0 +1,30 @@
+#include <TM1637Display.h>
+#include "SR04.h"
+
+// connections pins:
+#define CLK 2
+#define DIO 3
+#define buzzer 8
+#define TRIG_PIN 7
+#define ECHO_PIN 6
+
+// Create display object of type TM1637Display:
+TM1637Display display = TM1637Display(CLK, DIO);
+
+// Ultrasonic sensor
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  display.clear();
+  display.setBrightness(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long dist=sr04.Distance();
+  Serial.print(dist);
+  Serial.println(" cm"); 
+  display.showNumberDec(dist);   
+  delay(500);
+}
